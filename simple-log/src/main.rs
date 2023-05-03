9341014b72c6876a6da39d3967bb4d00e13b4dad
Rusty Quick <winterspark@rustys-mbp.lan>
2023-05-02 23:58:59 -0500

    doing a log writer thing.

--- simple-log/src/main.rs
+++ simple-log/src/main.rs
@@ -1,7 +1,30 @@
 extern crate chrono;
+
+use std::io::prelude::*;
+use std::fs::File;
+use std::io;
 use chrono::*;
 
-fn main() {
+fn log_something(filename: &'static str, string: &'static [u8; 12]) -> io::Result<()> {
+    let mut f = File::create(filename)?;
+    f.write_all(string)?;
+    Ok(())
+}
+
+fn log_time(filename: &'static str) -> io::Result<()> {
     let local: DateTime<Local> = Local::now();
-    println!("{}", local);
+    let formatted = local.format("%a, %b %d %Y %I:%M:%S %p\n").to_string();
+    let bytes = formatted.as_bytes();
+    let mut f = File::create(filename)?;
+    f.write_all(bytes)?;
+    Ok(())
+}
+
+fn main() {
+    // let local: DateTime<Local> = Local::now();
+    // println!("{}", local);
+    match log_time("newlog.txt") {
+        Ok(..) => println!("File created!"),
+        Err(..) => println!("Error: could not create file.")
+    }
 }
